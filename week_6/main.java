a25ce4a809630f4f18688adbaef2563cc9c5f1fd
8voux <ghaithhorimi150@gmail.com>
2026-06-30 05:32:48 +0000

    add to OOP week 6

---
+++ week_6/main.java
@@ -0,0 +1,15 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        Lecturer lecturer = new Lecturer(
+                "L100",
+                "Dr Ahmad",
+                "Java Programming",
+                "Faculty of Information Technology"
+        );
+
+        lecturer.displayInfo();
+        lecturer.displaySubject();
+    }
+}
